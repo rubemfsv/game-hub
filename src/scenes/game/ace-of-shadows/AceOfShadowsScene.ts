9ab219454082ea79993d3675554f1ec844fd4cd4
Rubem <rubemfsv15@gmail.com>
2025-07-12 06:48:59 -0300

    feat: create ace of shadows logic using card and deck

---
+++ src/scenes/game/ace-of-shadows/AceOfShadowsScene.ts
@@ -0,0 +1,44 @@
+import * as PIXI from 'pixi.js';
+import { BaseScene } from 'scenes/BaseScene';
+import { Deck } from './Deck';
+
+export class AceOfShadowsScene extends BaseScene {
+  private deck: Deck;
+  private discardPile: PIXI.Container;
+  private dealInterval: NodeJS.Timeout | null = null;
+
+  constructor(game: any) {
+    super(game, 'Ace of Shadows');
+
+    this.deck = new Deck(this.game.getApp().renderer);
+    this.discardPile = new PIXI.Container();
+
+    this.addChild(this.deck.container, this.discardPile);
+
+    this.startDealing();
+  }
+
+  private startDealing(): void {
+    this.dealInterval = setInterval(() => {
+      if (!this.deck.hasCards) {
+        if (this.dealInterval) clearInterval(this.dealInterval);
+        return;
+      }
+      this.deck.moveTopCard(this.discardPile);
+    }, 1000);
+  }
+
+  public onResize(width: number, height: number): void {
+    super.onResize(width, height);
+    this.deck.container.position.set(width * 0.25, height / 2);
+    this.discardPile.position.set(width * 0.75, height / 2);
+  }
+
+  public destroy(options?: PIXI.IDestroyOptions | boolean): void {
+    super.destroy(options);
+    if (this.dealInterval) {
+      clearInterval(this.dealInterval);
+    }
+    this.deck.destroy();
+  }
+}
